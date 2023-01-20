396c26459a23a5d23365f1280efe4a10381e21cd
Erik Heeren <erik.heeren@epfl.ch>
2023-01-20 11:37:28 +0100

    Improve error message

--- .github/workflows/validate-commit-message.py
+++ .github/workflows/validate-commit-message.py
@@ -18,8 +18,9 @@ for spack_repo in ['./var/spack/repos/builder.test',
                    './bluebrain/repo-patches']:
     existing_packages.extend(os.walk(f'{spack_repo}/packages'))
 
-if package not in existing_packages and package not in keywords:
-    msg = f'Commit message "{repo.head.commit.message}" does not follow the required template\n'
-    msg += f'{package} is not a known package or one of {keywords}\n'
-    msg += 'Please reformat your commit message'
+if package.strip() not in existing_packages and package not in keywords:
+    msg = f'Commit message "{repo.head.commit.message}" does not follow the required template.\n'
+    msg += f'"{package}" is not a known package or one of {keywords}\n'
+    msg += 'Please reformat your commit message to start with either a package name, '
+    msg += f'or one of {keywords} followed by a :'
     raise ValueError(msg)
