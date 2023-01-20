57edd477765306820d903144679bfe92958a1a53
Erik Heeren <erik.heeren@epfl.ch>
2023-01-20 13:06:52 +0100

    nopackage: iterate over commits in PR

--- .github/workflows/validate-commit-message.py
+++ .github/workflows/validate-commit-message.py
@@ -7,9 +7,13 @@ from git import Repo
 keywords = ['nopackage', 'deploy']
 
 repo = Repo('.')
-package = repo.head.commit.message.split(':')[0]
-existing_packages = []
+commit_packages = []
 
+for commit in repo.iter_commits():
+    commit_packages.append((commit.hexsha, commit.message.split(':')[0]))
+# package = repo.head.commit.message.split(':')[0]
+
+existing_packages = []
 for spack_repo in ['./var/spack/repos/builder.test',
                    './var/spack/repos/builtin',
                    './var/spack/repos/builtin.mock',
@@ -18,9 +22,14 @@ for spack_repo in ['./var/spack/repos/builder.test',
                    './bluebrain/repo-patches']:
     existing_packages.extend(os.walk(f'{spack_repo}/packages'))
 
-if package.strip() not in existing_packages and package not in keywords:
-    msg = f'Commit message "{repo.head.commit.message}" does not follow the required template.\n'
-    msg += f'"{package}" is not a known package or one of {keywords}\n'
-    msg += 'Please reformat your commit message to start with either a package name, '
-    msg += f'or one of {keywords} followed by a :'
-    raise ValueError(msg)
+faulty_commits = []
+for commit_info in commit_packages:
+    sha, package = commit_info
+    if package.strip() not in existing_packages and package not in keywords:
+        msg = f'Commit {sha} message "{repo.head.commit.message}" does not follow the required template.\n'
+        msg += f'"{package}" is not a known package or one of {keywords}\n'
+        msg += 'Please reformat your commit message to start with either a package name, '
+        msg += f'or one of {keywords} followed by a :'
+        faulty_commits.append(msg)
+
+raise ValueError('\n'.join(faulty_commits))
