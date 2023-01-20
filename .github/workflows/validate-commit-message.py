6605f71d41c4e778e5385b3eddfffcb2b9fe54f8
Erik Heeren <erik.heeren@epfl.ch>
2023-01-20 15:02:00 +0100

    Try pull_request.commits (#2)
    nopackage: merge experiment

--- .github/workflows/validate-commit-message.py
+++ .github/workflows/validate-commit-message.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# requires: gitpython
+
 import os
 
 from git import Repo
@@ -7,20 +9,35 @@ from git import Repo
 keywords = ['nopackage', 'deploy']
 
 repo = Repo('.')
-package = repo.head.commit.message.split(':')[0]
-existing_packages = []
 
+existing_packages = []
 for spack_repo in ['./var/spack/repos/builder.test',
                    './var/spack/repos/builtin',
                    './var/spack/repos/builtin.mock',
                    './var/spack/repos/tutorial',
                    './bluebrain/repo-bluebrain',
                    './bluebrain/repo-patches']:
-    existing_packages.extend(os.walk(f'{spack_repo}/packages'))
+    try:
+        existing_packages.extend(next(os.walk(f'{spack_repo}/packages'))[1])
+    except StopIteration:
+        print(f'No packages under {spack_repo}')
+        pass
 
-if package.strip() not in existing_packages and package not in keywords:
-    msg = f'Commit message "{repo.head.commit.message}" does not follow the required template.\n'
-    msg += f'"{package}" is not a known package or one of {keywords}\n'
-    msg += 'Please reformat your commit message to start with either a package name, '
-    msg += f'or one of {keywords} followed by a :'
-    raise ValueError(msg)
+faulty_commits = []
+for commit in repo.iter_commits():
+    print(f'Commit: {commit.message}')
+    if len(commit.parents) > 1:
+        print('Not going beyond a merge commit')
+        break
+
+    package = commit.message.split(':')[0]
+    if package.strip() not in existing_packages and package not in keywords:
+        msg = f'Commit {commit.hexsha} message "{commit.message.rstrip()}" does not follow the required template.\n'
+        msg += f'"{package}" is not a known package or one of {keywords}\n'
+        msg += 'Please amend your commit message to start with either a package name, '
+        msg += f'or one of {keywords} followed by a ":"\n'
+        msg += 30 * '='
+        faulty_commits.append(msg)
+
+if faulty_commits:
+    raise ValueError('\n'.join(faulty_commits))
