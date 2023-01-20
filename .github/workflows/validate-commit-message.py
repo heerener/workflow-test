a227e8e37e9945cb0f800df64c0819cb0f12e278
Erik Heeren <erik.heeren@epfl.ch>
2023-01-20 17:33:39 +0100

    nopackage: further comment formatting

--- .github/workflows/validate-commit-message.py
+++ .github/workflows/validate-commit-message.py
@@ -39,8 +39,9 @@ for commit in repo.iter_commits():
 
 if faulty_commits:
     warning = 'These commits are not formatted correctly. Please amend them to start with one of:\n'
-    warning += '* <package>: \n'
-    warning += f'* {", ".join(keywords)}'
+    warning += '* \\<package>: \n'
+    warning += f'* {", ".join(keywords)}\n\n'
+    warning += "### Faulty commits:\n"
     faulty_commits.insert(0, warning)
     with open('faulty_commits.txt', 'w') as fp:
         fp.write('\n'.join(faulty_commits))
